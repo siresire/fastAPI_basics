44c21f8a1dc7ec9c43cb58d0254302338a582ba7
Godfrey Bosire <godfreybosire@Godfreys-MacBook-Pro.local>
2023-10-20 16:02:41 +0300

    base-model

--- main.py
+++ main.py
@@ -1,8 +1,17 @@
+from typing import Optional
 from fastapi import FastAPI
+from pydantic import BaseModel
 
-# instance of the fastapi
 app = FastAPI()
 
+# the model of the data
+class Blog(BaseModel):
+    title : str
+    body  : str
+    published : Optional[bool]
+    
+    
+# instance of the fastapi
 
 # base path
 # : app is the path operation decorator
@@ -20,3 +29,21 @@ async def about():
     return {"data": {"message": "About page"}}
 
 
+# path parameters with dinamic routing 
+@app.get("/blog/{id}")
+async def show(id : int):
+    # fetch blog with id = id
+    return {"data": {id} }
+
+# path parameters with dinamic routing 
+@app.get("/blog/{id}/comments")
+async def comment(id : int ):
+    # fetch comment od blog with id = id
+    return {"data": "comments" }
+
+
+# post parameter is used to create something 
+# : Blog is the model of the data
+@app.post("/blog")
+async def create_blog(requests : Blog):
+    return {'data': f"blog is created with {requests.dict()}"}
