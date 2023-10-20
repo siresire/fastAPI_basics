3930ae8fe3130777dd986d1b49562c5acb7193de
Godfrey Bosire <godfreybosire@Godfreys-MacBook-Pro.local>
2023-10-20 14:50:13 +0300

    path operation

--- main.py
+++ main.py
@@ -4,7 +4,19 @@ from fastapi import FastAPI
 app = FastAPI()
 
 
-
+# base path
+# : app is the path operation decorator
+# : get is the operation on the path
 @app.get("/")
 async def index():
     return {"data": {"message": "Welcome to my FastAPI"}}
+
+# base path operation function
+
+
+# path parameters about 
+@app.get("/about")
+async def about():
+    return {"data": {"message": "About page"}}
+
+
