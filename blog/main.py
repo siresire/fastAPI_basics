52b5997225a10e9a283cc6d3377ba72778baa59d
Godfrey Bosire <godfreybosire@Godfreys-MacBook-Pro.local>
2023-10-20 18:36:13 +0300

    schemas

--- blog/main.py
+++ blog/main.py
@@ -1,9 +1,14 @@
 from fastapi import FastAPI
 
+from . import schemas
+
+
 app = FastAPI()
+
+
 
 
 # Start by creatign a post 
 @app.post('/blog')
-async def create_blog():
-    return "Creating "
+async def create_blog(request : schemas.Blog):
+    return request
